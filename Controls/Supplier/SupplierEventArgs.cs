644fcf4712d7dbe78bbc6a2e939455dd1d00e1fb
Willie Burger <willie@rtmsoft.co.za>
2017-01-23 10:10:57 +0200

    Supplier Lookup Wireframe

---
+++ Controls/Supplier/SupplierEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astrodon.Controls.Supplier
+{
+    public delegate void SupplierSelectedEventHandler(object sender, SupplierEventArgs e);
+
+    public class SupplierEventArgs : EventArgs
+    {
+        public SupplierEventArgs(Astrodon.Data.SupplierData.Supplier supplier)
+        {
+            SelectedItem = supplier;
+            SupplierSelected = true;
+        }
+
+        public SupplierEventArgs()
+        {
+            SupplierSelected = false;
+        }
+
+        public Astrodon.Data.SupplierData.Supplier SelectedItem { get; set; }
+
+        public bool SupplierSelected { get; private set; }
+    }
+}
---
+++ Controls/Supplier/usrSupplierLookup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Astrodon.Data;
+
+namespace Astrodon.Controls.Supplier
+{
+
+    public partial class usrSupplierLookup : UserControl
+    {
+        public usrSupplierLookup(DataContext context)
+        {
+            InitializeComponent();
+        }
+
+        #region Supplier Lookup Events
+        public event SupplierSelectedEventHandler SupplierSelectedEvent;
+
+        private void SupplierSelected(Astrodon.Data.SupplierData.Supplier selectedItem)
+        {
+            if (SupplierSelectedEvent != null)
+                SupplierSelectedEvent(this, new SupplierEventArgs(selectedItem));
+        }
+
+        private void SupplierCancelled()
+        {
+            if (SupplierSelectedEvent != null)
+                SupplierSelectedEvent(this, new SupplierEventArgs());
+        }
+        #endregion
+    }
+}
