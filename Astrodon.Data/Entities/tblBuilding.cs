162ed43d30cd5dd974a233ddbc55087a432d8477
Willie Burger <willie@rtmsoft.co.za>
2017-08-24 14:12:38 +0200

    Updated Building Table. Added Migrations for missing changes

--- Astrodon.Data/Entities/tblBuilding.cs
+++ Astrodon.Data/Entities/tblBuilding.cs
@@ -86,6 +86,12 @@ namespace Astrodon.Data
 
         public bool hoa { get; set; }
 
+        public string limitM { get; set; }
+
+        public string limitW { get; set; }
+
+        public string limitD { get; set; }
+
         #region Building Insurance
 
         public decimal CommonPropertyDimensions { get; set; }
---
+++ Astrodon.Data/Migrations/201708241155033_AddedMissingPropertiesForBuildings.cs
@@ -0,0 +1,28 @@
+namespace Astrodon.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedMissingPropertiesForBuildings : DbMigration
+    {
+        public override void Up()
+        {
+            try
+            {
+                AddColumn("dbo.tblBuildings", "limitM", c => c.String());
+                AddColumn("dbo.tblBuildings", "limitW", c => c.String());
+                AddColumn("dbo.tblBuildings", "limitD", c => c.String());
+            }
+            catch (Exception)
+            {
+            }
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tblBuildings", "limitD");
+            DropColumn("dbo.tblBuildings", "limitW");
+            DropColumn("dbo.tblBuildings", "limitM");
+        }
+    }
+}
